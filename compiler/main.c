d94d9ee18101a70febdce91149a1f5ba4d130330
Florian MOREL <m.morel.florian@gmail.com>
2018-03-22 23:11:22 +0100

    Interpreter + basic Ast parser done

--- compiler/main.c
+++ compiler/main.c
@@ -1,3 +1,8 @@
+//TODO(Florian): Handle error properly when EOF is there
+//TODO(Florian): Handle eating whitespace
+//TODO(Florian): refactor TokenKind to use directly char for OP, as shown by Per Vognsen for bitwise
+//TODO(Florian): 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -230,19 +235,15 @@ bool expect_token(TokenKind kind) {
     }
 }
 
-s64 res = 0;
-
-
 #if 0
 expr3 = VAL
 expr2 = [-~] expr3
 expr1 = expr2 ([*/%&] expr2)*
 expr0 = expr1 ([+-|^] expr1)*
 expr = expr0
-
 #endif
 
-// TODO(Florian): Implement grouping
+// TODO(Florian): Implement grouping with '(' and ')'
 s64 inter_parse_expr3() {
     if (!is_token(TOKENKIND_VAL)) {
         perror("Not expect tokenkind in depth3");
@@ -265,7 +266,7 @@ s64 inter_parse_expr2() {
 
 s64 inter_parse_expr1() {
     s64 val = inter_parse_expr2();
-    while (is_token(TOKENKIND_MUL) || is_token(TOKENKIND_DIV) || is_token(TOKENKIND_MOD) || is_token(TOKENKIND_AND)) {
+    while (is_token(TOKENKIND_MUL) || is_token(TOKENKIND_DIV) || is_token(TOKENKIND_MOD) || is_token(TOKENKIND_AND) || is_token(TOKENKIND_SHFTL) || is_token(TOKENKIND_SHFTR)) {
         
         if (match_token(TOKENKIND_MUL)){
             val *= inter_parse_expr2();
@@ -281,6 +282,14 @@ s64 inter_parse_expr1() {
         
         if(match_token(TOKENKIND_AND)) {
             val &= inter_parse_expr2();
+        }
+        
+        if(match_token(TOKENKIND_SHFTL)) {
+            val <<= inter_parse_expr2();
+        }
+        
+        if(match_token(TOKENKIND_SHFTR)) {
+            val >>= inter_parse_expr2();
         }
     }
     return val;
@@ -356,25 +365,191 @@ void lex_test() {
     lex_test_2();
 }
 
+void stream_init(const char* expr) {
+    stream = expr;
+    next_token();
+}
+
 void inter_test() {
-    stream = "12";
-    next_token();
+    stream_init("12");
     assert(inter_parse_expr() == 12);
-    stream = "12+13";
-    next_token();
+    stream_init("12+13");
     assert(inter_parse_expr() == 25);
-    stream = "2*3+4";
-    next_token();
+    stream_init("2*3+4");
     assert(inter_parse_expr() == 10);
-    stream = "2*2-4*4/8";
-    next_token();
+    stream_init("2*2-4*4/8");
     assert(inter_parse_expr() == 2);
+}
+
+typedef enum NodeType {
+    NODETYPE_OP,
+    NODETYPE_VAL,
+} NodeType;
+
+typedef struct Ast_Node {
+    NodeType type;
+    union {
+        s64 val;
+        char op;
+    };
+    struct Ast_Node *left;
+    struct Ast_Node *right;
+}Ast_Node;
+
+typedef struct Ast {
+    Ast_Node root;
+} Ast;
+
+Ast_Node* make_op(char op) {
+    Ast_Node* result = malloc(sizeof(Ast_Node));
+    result->type = NODETYPE_OP;
+    result->op = op;
+    result->left = result->right = NULL;
+    return result;
+}
+
+Ast_Node* make_val(s64 val) {
+    Ast_Node* result = malloc(sizeof(Ast_Node));
+    result->type = NODETYPE_VAL;
+    result->val = val;
+    result->left = result->right = NULL;
+    return result;
+}
+
+Ast_Node* parse_expr3() {
+    assert(is_token(TOKENKIND_VAL));
+    Ast_Node* result = make_val(token.val);
+    next_token();
+    return result;
+}
+
+Ast_Node* parse_expr2() {
+    Ast_Node* op_node = NULL;
+    if (match_token(TOKENKIND_MIN)) {
+        op_node = make_op('-');
+    }
+    if (match_token(TOKENKIND_COMPL)) {
+        op_node = make_op('~');
+    }
+    Ast_Node* n = parse_expr3();
+    if (op_node != NULL) {
+        op_node->right = n;
+        return op_node;
+    }
+    return n;
+}
+
+Ast_Node* parse_expr1() {
+    Ast_Node* n = parse_expr2();
+    Ast_Node* result = n;
+    Ast_Node* left = n;
+    
+    while (is_token(TOKENKIND_MUL) || is_token(TOKENKIND_DIV) || is_token(TOKENKIND_MOD) || 
+           is_token(TOKENKIND_AND) || is_token(TOKENKIND_SHFTL) || is_token(TOKENKIND_SHFTR)) {
+        
+        char op;
+        
+        if(match_token(TOKENKIND_MUL)) {
+            op = '*';
+        }
+        
+        if(match_token(TOKENKIND_DIV)) {
+            op = '/';
+        }
+        
+        if(match_token(TOKENKIND_MOD)) {
+            op = '%';
+        }
+        
+        if(match_token(TOKENKIND_AND)) {
+            op = '&';
+        }
+        
+        if(match_token(TOKENKIND_SHFTL)) {
+            //TODO(Florian): Handle string instead of char
+            op = '<';
+        }
+        
+        if(match_token(TOKENKIND_SHFTR)) {
+            //TODO(Florian): Handle string instead of char
+            op = '>';
+        }
+        
+        result = make_op(op);
+        result->left = left;
+        result->right = parse_expr2();
+        left = result;
+    }
+    return result;
+}
+
+Ast_Node* parse_expr0() {
+    Ast_Node* n = parse_expr1();
+    Ast_Node* result = n;
+    Ast_Node* left = n;
+    while (is_token(TOKENKIND_ADD) || is_token(TOKENKIND_MIN) || is_token(TOKENKIND_OR) || is_token(TOKENKIND_XOR)) {
+        
+        char op;
+        
+        if(match_token(TOKENKIND_ADD)) {
+            op = '+';
+            
+        }
+        
+        if(match_token(TOKENKIND_MIN)) {
+            op = '-';
+        }
+        
+        if(match_token(TOKENKIND_OR)) {
+            op = '|';
+        }
+        
+        if(match_token(TOKENKIND_XOR)) {
+            op = '^';
+        }
+        
+        result = make_op(op);
+        result->left = left;
+        result->right = parse_expr1();
+        left = result;
+    }
+    return result;
+}
+
+Ast_Node* parse_expr() {
+    return parse_expr0();
+}
+
+void print_Ast(Ast_Node *n) {
+    switch (n->type) {
+        case NODETYPE_OP: {
+            printf("(%c ", n->op);
+            if (n->left) { print_Ast(n->left); }
+            if (n->right) { print_Ast(n->right); }
+            printf(")");
+        } break;
+        case NODETYPE_VAL: {
+            printf("%lld ", n->val);
+        }break;
+    }
+}
+
+void parse_test() {
+    printf("\n");
+    stream_init("28*8+19/7-15");
+    Ast_Node* r = parse_expr();
+    print_Ast(r);
+    printf("\n");
+    stream_init("1<<5>>2");
+    r = parse_expr();
+    print_Ast(r);
 }
 
 void do_test() {
     buf_test();
     lex_test();
     inter_test();
+    parse_test();
 }
 
 int main(int argc, char** argv) {
